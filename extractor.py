16209ba36979ef8fe85d56c93810a722fae089d6
Nada Amin <namin@seas.harvard.edu>
2023-10-11 00:27:17 -0400

    some tweaks -- using llama now too

--- extractor.py
+++ extractor.py
@@ -2,11 +2,15 @@ from markers import postambule
 import outlines.text.generate as generate
 import outlines.models as models
 
-base_model_name = "mistralai/Mistral-7B-v0.1"
+#base_model_name = "mistralai/Mistral-7B-v0.1"
+#base_model_name = "meta-llama/Llama-2-70b-hf" # takes forever!
+base_model_name = "meta-llama/Llama-2-13b-hf"
 model = models.transformers(base_model_name)
 
 def extract(r, choices):
     i = r.index(postambule)
     prompt = r[i+len(postambule):]
+    print(prompt)
+    print("The suspects are "+",".join(choices))
     prompt += "\nThe culprit is"
     return generate.choice(model, choices)(prompt)
--- llm.py
+++ llm.py
@@ -1,7 +1,8 @@
 import torch
 from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer, TextStreamer
 
-base_model_name = "mistralai/Mistral-7B-v0.1"
+#base_model_name = "mistralai/Mistral-7B-v0.1"
+base_model_name = "meta-llama/Llama-2-70b-hf"
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
@@ -22,12 +23,15 @@ model = base_model
 
 streamer = TextStreamer(tokenizer)
 
-def generate(eval_prompt, max_new_tokens=50):
+def generate(eval_prompt, max_new_tokens=200):
     model_input = tokenizer(eval_prompt, return_tensors="pt").to("cuda")
     
     model.eval()
     
     r = None
     with torch.no_grad():
-        r = tokenizer.decode(model.generate(**model_input, streamer=streamer, max_new_tokens=max_new_tokens)[0], skip_special_tokens=True)
+        r = tokenizer.decode(model.generate(**model_input,
+                                            #streamer=streamer,
+                                            max_new_tokens=max_new_tokens)[0],
+                             skip_special_tokens=True)
     return r

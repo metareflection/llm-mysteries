377fe050050339f8f499c75cf281dee3c9afca6e
Nada Amin <namin@seas.harvard.edu>
2023-10-12 19:11:43 -0400

    use greedy choice

--- extractor.py
+++ extractor.py
@@ -1,4 +1,5 @@
 from markers import postambule
+import outlines.text.generate.sample as sample
 import outlines.text.generate as generate
 import outlines.models as models
 import torch
@@ -7,9 +8,6 @@ import torch
 #base_model_name = "meta-llama/Llama-2-70b-hf" # takes forever!
 base_model_name = "meta-llama/Llama-2-13b-hf"
 model = models.transformers(base_model_name, device="cuda")
-
-rng = torch.Generator(device="cuda")
-rng.manual_seed(789001)
 
 def extract(r, choices):
     i = r.index(postambule)
@@ -23,9 +21,7 @@ def gen(prompt, choices):
     choices_repr = [repr(c) for c in choices]
     choices_repr_joined = ', '.join(choices_repr)
     print(f'gen({repr(prompt)}, [{choices_repr_joined}])')
-    rng = torch.Generator(device="cuda")
-    rng.manual_seed(789001)
-    return gen_fresh(prompt, choices, rng=rng)
+    return gen_greedy(prompt, choices)
 
-def gen_fresh(prompt, choices, rng=rng):
-    return generate.choice(model, choices)(prompt, rng=rng)
+def gen_greedy(prompt, choices):
+    return generate.choice(model, choices, sampler=sample.greedy)(prompt)

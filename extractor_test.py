46d2072915a48ce9e1249d19775ba1e572074862
Nada Amin <namin@seas.harvard.edu>
2023-10-12 19:24:33 -0400

    cleanup, get rid of most repros

--- extractor_test.py
+++ extractor_test.py
@@ -1,16 +1,19 @@
 from extractor import gen
 
 prompt = """The culprit is Foo. Foo was sitting on the chair closest to the cupcake."""
-prompt += "\nThe culprit is"
+prompt += "\nSo as explained, the culprit is"
 choices = ["Foo", "Bar", "Baz", "Foobar"]
-print(gen(prompt, choices))
-# Should return Foo, but seems to return the others rather frequently too.
+answer = gen(prompt, choices)
+print(answer)
+assert answer == 'Foo'
 
 d = {}
 for c in choices:
     d[c] = 0
 
 for i in range(0, 100):
-    d[gen(prompt, choices)] += 1
+    answer = gen(prompt, choices)
+    d[answer] += 1
+    print(answer)
 print(d)
-# {'Foo': 0, 'Bar': 0, 'Baz': 0, 'Foobar': 100}
+assert d['Foo'] == 100

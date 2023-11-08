ded1fb495c4a85369c576320e8562b5fd6ea0a80
Nada Amin <namin@seas.harvard.edu>
2023-11-07 19:51:22 -0500

    load in low mode

--- extractor.py
+++ extractor.py
@@ -3,12 +3,30 @@ import outlines.text.generate.sample as sample
 import outlines.text.generate as generate
 import outlines.models as models
 import torch
+from transformers import BitsAndBytesConfig
 
 #base_model_name = "mistralai/Mistral-7B-v0.1"
 #base_model_name = "meta-llama/Llama-2-70b-hf" # takes forever!
 #base_model_name = "meta-llama/Llama-2-13b-hf"
 base_model_name = "meta-llama/Llama-2-13b-chat-hf"
-model = models.transformers(base_model_name, device="cuda")
+
+#model = models.transformers(base_model_name, device="cuda")
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+)
+
+model = models.transformers(
+    base_model_name,
+    device="auto",
+    model_kwargs={
+        "quantization_config": bnb_config,
+        #"device_map":"auto",
+        "trust_remote_code": True,
+        "use_auth_token": True,
+    },
+)
 
 def extract(r, choices):
     i = r.index(postambule)

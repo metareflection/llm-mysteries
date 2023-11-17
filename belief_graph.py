9eb2d970f803109c2a7998551ffaebf0ef103ef5
Nada Amin <namin@seas.harvard.edu>
2023-11-17 05:59:31 -0500

    done but not normalized

---
+++ belief_graph.py
@@ -0,0 +1,58 @@
+from extractor import model
+from sequence_probabilities import make_greedy_tracker
+from outlines.text.generate.regex import choice
+from math import exp
+from load import dataset, suspects, culprit, tagline, story_text
+import mystery
+
+bool_choices = ['True', 'False']
+
+belief_generator = make_greedy_tracker(
+    choice(model, bool_choices, max_tokens=50))
+
+def belief_probability(prompt):
+    question = prompt[0:prompt.index('?')+1]
+    print(question)
+    val = belief_generator(prompt)
+    confidence = exp(belief_generator.sequence_log_prob)
+    print(f"{val} ({confidence})")
+    return (val, confidence)
+
+def create_prompt(story, id, neg, what=None):
+    postfix = ': True or False?'
+    if what is None:
+        prompt = f"{id} is{' not' if neg else ''} guilty"
+    else:
+        prompt = f"{id} has{' no' if neg else ''} mean"
+    question = prompt + postfix
+    return question + '\n' + story + '\n' + question
+
+def create_story_lines(story, suspect_list):
+    lines = []
+    for suspect in suspect_list:
+        for what in [None] + mystery.whats:
+            #for neg in [True, False]:
+            neg = False
+            (val, confidence) = belief_probability(create_prompt(story, suspect, neg, what))
+            lines.append((suspect, what, val, confidence))
+    return lines
+
+def solve(story, suspect_list):
+    return mystery.solve(create_story_lines(story, suspect_list))
+
+def processCase(x):
+    print(f"## {tagline(x)}")
+    found_culprit = solve(story_text(x), suspects(x))
+    print(f"The culprit is {found_culprit}.")
+    print(f"In fact, it is {culprit(x)}.")
+    x['eval'] = found_culprit == culprit(x)
+    return x
+
+def processAll():
+    results = dataset.map(processCase)
+    solved = len(list(1 for e in results['train']['eval'] if e==1))
+    total = results.num_rows['train']
+    print(f"Solved {solved} out of {total}.")
+
+if __name__ == '__main__':
+    processAll()
---
+++ sequence_probabilities.py
@@ -0,0 +1,66 @@
+import torch
+
+import outlines.models as models
+
+from outlines.text.generate.regex import choice
+from outlines.text.generate.continuation import continuation
+from outlines.text.generate.sample import greedy
+
+
+def make_greedy_tracker(generator):
+    import types
+
+    generator.sequence_log_prob = 0.0
+
+    def tracking_greedy(
+        logits: torch.DoubleTensor, samples: int, *_
+    ) -> torch.DoubleTensor:
+        next_token_ids = greedy(logits, samples)
+
+        probs = torch.nn.functional.softmax(logits, dim=-1)
+        generator.sequence_log_prob += torch.log(
+            probs[:, next_token_ids.squeeze()].squeeze()
+        )
+
+        return next_token_ids
+
+    generator.sampler = tracking_greedy
+
+    def new_call(self, *args, **kwargs):
+        # Reset the sequence log-probability
+        self.sequence_log_prob = 0.0
+        return super().__call__(*args, **kwargs)
+
+    generator.__call__ = types.MethodType(generator, new_call)
+
+    return generator
+
+
+if __name__ == '__main__':
+    model = models.transformers("gpt2")
+    generator = make_greedy_tracker(continuation(model, max_tokens=50))
+    choice_generator = make_greedy_tracker(
+        choice(model, ["[Bb]lue", "[Rr]ed"], max_tokens=50)
+    )
+    prompt = "Which color do you prefer: blue or red?"
+
+    sequence = generator(prompt)
+    print(sequence)
+    #
+    #
+    # The answer is blue.
+    #
+    # The color of the car is the color of the car.
+    #
+    # The color of the car is the color of the car.
+    #
+    # The color of the car is the color of the car.
+    print(generator.sequence_log_prob)
+    # tensor(-44.7725)
+
+
+    sequence = choice_generator(prompt)
+    print(sequence)
+    # Blue
+    print(choice_generator.sequence_log_prob)
+    # tensor(-0.9262)

d3d7bc16a2c4159d66c1591a81256205ce8826e4
Nada Amin <namin@seas.harvard.edu>
2023-11-17 04:51:42 -0500

    encoded

--- mystery.py
+++ mystery.py
@@ -23,8 +23,6 @@ Maurice Eades has opportunity: he is a servant.
 Maurice Eades has no motive: he is trusted!!!!!.
 """.strip()
 
-story_lines = story.split('\n')
-
 re_line = re.compile(r'(?P<id>\w+ \w+) has ((?P<neg>no) )?(?P<what>mean|opportunity|motive): (.*?)(?P<bangs>!*).')
 
 def get_init(d, key):
@@ -55,19 +53,54 @@ def complete_graph(suspects):
                     d2[val] = 0.0
     return suspects
 
-suspects = complete_graph(init_graph(story_lines))
+def suspect_var(id):
+    return f"{id} is guilty"
+
+def what_var(id, what):
+    return f"{id} has {what}"
 
 def create_vars(suspects):
     vars = {}
     def add_var(s):
         vars[s] = Bool(s)
     for (id,d) in suspects.items():
-        add_var(f"{id} is guilty")
+        add_var(suspect_var(id))
         for what in whats:
-            add_var(f"{id} has {what}")
+            add_var(what_var(id, what))
     return vars
 
-vars = create_vars(suspects)
+def add_soft_constraints(s, vars, suspects):
+    for (id,d) in suspects.items():
+        for what in whats:
+           v = vars[what_var(id, what)]
+           s.add_soft(Not(v), exp(-d[what][True]))
+           s.add_soft(v, exp(-d[what][False]))
 
+def add_hard_constraints(s, vars, suspects):
+    xor_expr = Sum([If(vars[suspect_var(id)], 1, 0) for id in suspects.keys()]) == 1
+    s.add(xor_expr)
+    for (id,d) in suspects.items():
+        var_id = vars[suspect_var(id)]
+        all_whats = And(*[vars[what_var(id, what)] for what in whats])
+        s.add(Implies(all_whats, var_id))
+        s.add(Implies(Not(all_whats), Not(var_id)))
 
-            
+def find_guilty_suspect(model, vars, suspects):
+    for id in suspects.keys():
+        if model[vars[suspect_var(id)]]:
+            return id
+    assert False
+
+def main(story_lines):
+    suspects = complete_graph(init_graph(story_lines))
+    vars = create_vars(suspects)
+    s = Optimize()
+    add_soft_constraints(s, vars, suspects)
+    add_hard_constraints(s, vars, suspects)
+    assert s.check() == sat
+    model = s.model()
+    print(find_guilty_suspect(model, vars, suspects))
+
+if __name__ == '__main__':
+    story_lines = story.split('\n')
+    main(story_lines)

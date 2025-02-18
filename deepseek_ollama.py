a81963163d754015a2c3ad34506a795bc78276b4
Nada Amin <namin@seas.harvard.edu>
2025-02-17 19:05:25 -0500

    back to deepseek

--- deepseek_ollama.py
+++ deepseek_ollama.py
@@ -1,8 +1,8 @@
 import ollama
 
 MODEL='deepseek-r1'
-MODEL='llama3.3'
-MODEL='phi4'
+#MODEL='llama3.3'
+#MODEL='phi4'
 
 def generate(prompt):
     r = ollama.generate(model=MODEL, prompt=prompt, options={'temperature':0.6})
